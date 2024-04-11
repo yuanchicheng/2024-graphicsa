edc52929fe41bdeb7e4d2c0452367966b44404f8
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2024-04-11 16:54:22 +0800

    week08

---
+++ week08-1_glm_gundam/main.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+void drawmodel(void)
+{
+    if (!pmodel) {
+	pmodel = glmReadOBJ("data/Gundam.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    drawmodel();//glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])///138¦æ
+{
+  glutInit(&argc, argv);///140¦æ
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);///143¦æ
+  glutCreateWindow("week08 gundam");///145¦æ
+  glutDisplayFunc(display);///148¦æ
+
+  glutMainLoop();///174¦æ
+
+}
